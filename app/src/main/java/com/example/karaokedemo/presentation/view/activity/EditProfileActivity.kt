68e31d7d3e6ae70827784f7f387220258dc11620
Diego <diego200039@gmail.com>
2020-05-04 11:14:40 -0500

    Edit profile

--- app/src/main/java/com/example/karaokedemo/presentation/view/activity/EditProfileActivity.kt
+++ app/src/main/java/com/example/karaokedemo/presentation/view/activity/EditProfileActivity.kt
@@ -1,7 +1,10 @@
 package com.example.karaokedemo.presentation.view.activity
 
+import android.Manifest
 import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.database.Cursor
 import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
@@ -10,15 +13,22 @@ import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import com.example.karaokedemo.R
 import com.example.karaokedemo.databinding.ActivityEditProfileBinding
+import com.example.karaokedemo.presentation.view.util.withError
 import com.example.karaokedemo.presentation.viewmodel.EditProfileViewModel
+import com.google.android.material.snackbar.Snackbar
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
 import java.io.IOException
+import java.nio.channels.FileChannel
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -28,16 +38,19 @@ class EditProfileActivity : AppCompatActivity() {
     companion object {
         const val REQUEST_TAKE_PHOTO = 1
         const val REQUEST_PICK_IMAGE = 2
+        const val REQUEST_PERMISSION_WRITE_STORAGE = 3
     }
 
     private lateinit var binding: ActivityEditProfileBinding
     private val viewModel: EditProfileViewModel by viewModel()
-    private lateinit var currentPhotoPath: String
+    private var currentPhotoUri: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_close)
+        supportActionBar?.setBackgroundDrawable(ContextCompat.getDrawable(this, android.R.color.transparent))
+        supportActionBar?.title = getString(R.string.lbl_edit_profile)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_edit_profile)
         binding.viewModel = viewModel
         binding.lifecycleOwner = this
@@ -58,10 +71,10 @@ class EditProfileActivity : AppCompatActivity() {
         val sourceOptions = arrayOf(cameraOption, galleryOption)
         AlertDialog.Builder(this)
             .setTitle(R.string.lbl_profile_picture)
-            .setItems(sourceOptions) { a, index ->
+            .setItems(sourceOptions) { _, index ->
                 when (sourceOptions[index]) {
                     cameraOption -> takePhoto()
-                    galleryOption -> pickImage()
+                    galleryOption -> checkCanPickImage()
                 }
             }
             .show()
@@ -81,7 +94,7 @@ class EditProfileActivity : AppCompatActivity() {
                         "com.example.karaokedemo.fileprovider",
                         it
                     )
-                    currentPhotoPath = photoURI.toString()
+                    currentPhotoUri = photoURI.toString()
                     takePhotoIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
                     startActivityForResult(takePhotoIntent, REQUEST_TAKE_PHOTO)
                 }
@@ -100,6 +113,26 @@ class EditProfileActivity : AppCompatActivity() {
         )
     }
 
+    private fun checkCanPickImage() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                this, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), REQUEST_PERMISSION_WRITE_STORAGE)
+        } else {
+            pickImage()
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when (requestCode) {
+            REQUEST_PERMISSION_WRITE_STORAGE -> {
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    pickImage()
+                }
+            }
+        }
+    }
+
     private fun pickImage() {
         val intent = Intent(Intent.ACTION_PICK)
         intent.type = "image/*"
@@ -113,13 +146,12 @@ class EditProfileActivity : AppCompatActivity() {
         when (requestCode) {
             REQUEST_TAKE_PHOTO -> {
                 if (resultCode == Activity.RESULT_OK) {
-                    viewModel.profileImageUri.value = currentPhotoPath
+                    viewModel.profileImageUri.value = currentPhotoUri
                 }
             }
             REQUEST_PICK_IMAGE -> {
                 if (resultCode == Activity.RESULT_OK) {
-                    val imageUri = data?.data?.toString()
-                    viewModel.profileImageUri.value = imageUri
+                    data?.data?.let { saveImageFromGallery(it) }
                 }
             }
         }
@@ -128,6 +160,49 @@ class EditProfileActivity : AppCompatActivity() {
     private fun onUserSaved() {
         setResult(Activity.RESULT_OK)
         finish()
+    }
+
+    private fun saveImageFromGallery(selectedImageUri: Uri) {
+        val filePathColumn = arrayOf(MediaStore.Images.Media.DATA)
+        val cursor: Cursor? =
+            contentResolver.query(selectedImageUri, filePathColumn, null, null, null)
+        cursor ?: run {
+            showErrorMessage(getString(R.string.msg_general_error))
+            return
+        }
+
+        cursor.moveToFirst()
+        val columnIndex: Int = cursor.getColumnIndex(filePathColumn[0])
+
+        val imagePath: String = cursor.getString(columnIndex)
+
+        cursor.close()
+
+        var source: FileChannel? = null
+        var destination: FileChannel? = null
+        try {
+            val newFile = createImageFile()
+            source = FileInputStream(imagePath).channel
+            destination = FileOutputStream(newFile).channel
+            destination.transferFrom(source, 0, source.size())
+            val imageUri: Uri = FileProvider.getUriForFile(
+                this,
+                "com.example.karaokedemo.fileprovider",
+                newFile
+            )
+            viewModel.profileImageUri.value = imageUri.toString()
+        } catch (e: IOException){
+            showErrorMessage(getString(R.string.msg_general_error))
+        } finally {
+            source?.close()
+            destination?.close()
+        }
+    }
+
+    private fun showErrorMessage(message: String) {
+        Snackbar.make(binding.root, message, Snackbar.LENGTH_LONG)
+            .withError()
+            .show()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
